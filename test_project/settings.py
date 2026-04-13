3ea8499d4a703cc24955b50b22dd3ea4dea81218
Ruslan Toister <rtoister.cv@gmail.com>
2026-04-13 15:05:43 +0300

    add STATIC_ROOT

--- test_project/settings.py
+++ test_project/settings.py
@@ -66,5 +66,6 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 ASGI_APPLICATION = "test_project.asgi.application"
